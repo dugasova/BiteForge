ce712a864d794d59f4549f364e5115c3611952ec
Nataly Dugasova <dugasova@gmail.com>
2026-06-09 12:57:43 +0000

    refactor: simplify Input component by extending standard InputHTMLAttributes and spreading props

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,26 +1,14 @@
-import React, { forwardRef } from 'react';
-import './Input.scss';
+import { forwardRef, type InputHTMLAttributes } from 'react';
 
-type InputProps = {
-  type: string;
-  placeholder: string;
-  value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  onBlur: (e: React.FocusEvent<HTMLInputElement>) => void;
-  className?: string; // Add optional className
-}
+type InputProps = InputHTMLAttributes<HTMLInputElement>;
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ type, placeholder, value, onChange, onBlur, className }, ref) => {
+  ({ className, ...props }, ref) => {
     return (
       <input
-        className={`input ${type === 'email' ? 'email' : 'password'} ${className || ''}`}
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        onBlur={onBlur}
+        className={`input${className ? ` ${className}` : ''}`}
         ref={ref}
+        {...props}
       />
     );
   }
